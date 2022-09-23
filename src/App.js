36c3efcae5c8cf7871e849e62a478fd220f8566a
Thomas Sankara <thomassankara74@gmail.com>
2022-09-23 12:09:46 +0300

    change default image

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Lightbox from "./components/Lightbox";
 
 function App() {
   const [products] = useState(data);
-  const [value, setValue] = useState(1);
+  const [value, setValue] = useState(0);
   const [amount, setAmount] = useState(0);
   const [slideIndex, setSlideIndex] = useState(1);
   const [showLightbox, setShowLightbox] = useState(false);
